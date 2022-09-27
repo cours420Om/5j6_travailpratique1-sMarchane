95b846ffa912b02d45cd8659da314725d6733c94
Sif <sMarchane>
2022-09-27 15:26:32 -0400

    Interface activité d'accueil complété.

--- app/src/main/java/com/example/tp1/MainActivity.java
+++ app/src/main/java/com/example/tp1/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.tp1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Spinner sp_liste = (Spinner) findViewById(R.id.spin_liste);
+        ArrayList<String> listRestau = new ArrayList<>();
+        listRestau.add("MacDonalds");
+        listRestau.add("A&W");
+        listRestau.add("Harveys");
+
+        ArrayAdapter<String> adaptateur = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_dropdown_item, listRestau);
+
+        sp_liste.setAdapter(adaptateur);
     }
 }
